db0ec0bae2af8096c882314d0d4c005b0c19b8e7
sashimi <fun_hao@outlook.com>
2025-05-16 14:54:48 +1200

    進階題目，增加計算平均數，以及讓user自己輸入學生和成績

--- CPP learning/week3/map_intro.cpp
+++ CPP learning/week3/map_intro.cpp
@@ -1,22 +1,51 @@
 #include<iostream>
 #include<map>
+#include<iomanip>
 using namespace std;
 
 int main(){
     map<string, int>scores;
-    scores["Alice"] = 95;
-    scores["Bob"] = 88;
-    scores["Cathy"] = 76;
-
     string name;
-    cout << "Please enter a student name: ";
-    cin >> name;
+    int score;
+   // 原本是用while(name != "done")來判斷,現在改成用true和if來判斷;
+    while (true){
+        cout << "Enter student name (or 'done' to finish): ";
+        cin >> name;
+        if(name == "done")break;
 
-    if(scores.find(name) != scores.end()){
-        cout << name << "'s score is: " << scores[name] << endl;
+        cout << name << "'s score: ";
+        cin >> score;
+        scores[name] = score;
+    }
+
+    string n;
+    cout << "Please enter a student name to search: ";
+    cin >> n;
+
+    if(scores.find(n) != scores.end()){
+        cout << n << "'s score is: " << scores[n] << endl;
     }
     else{
         cout << "Student not found." << endl;
     }
+
+    int sum=0;
+    for(const auto& entry : scores){
+        sum += entry.second;
+    }
+
+    double average = 0.0;
+    if(!scores.empty()){
+        average = static_cast<double>(sum) / scores.size();// 避免整數除以整數,結果只取整數，保留小數
+    }
+    cout << fixed << setprecision(1);//保留小數一位
+    cout << "average score: " << average << endl;
+    
+
+    cout << "\nAll scores:\n";
+    for(const auto& entry : scores){
+        cout << entry.first << " - " << entry.second << endl;
+    }
+
     return 0;
 }
